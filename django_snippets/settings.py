07e60b2606e17b3ae752ce97afafe5d420380d0a
Miguel Puente <itpuentemiguelangel@gmail.com>
2025-03-12 05:19:41 -0300

    Add MySQL support for deployment

--- django_snippets/settings.py
+++ django_snippets/settings.py
@@ -74,10 +74,18 @@ WSGI_APPLICATION = "django_snippets.wsgi.application"
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),  # Puerto predeterminado para MySQL
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'connect_timeout': 30,
+        },
+    },
 }
 
 # Password validation
